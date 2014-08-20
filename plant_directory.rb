961080a0c883a5d2a7214450bc5d0b55707ed384
Griffin Wimminger <student@epicodus.com>
2014-08-20 15:42:31 -0700

    allow user to add and delete traits to and from a plant

--- plant_directory.rb
+++ plant_directory.rb
@@ -47,24 +47,27 @@ end
 
 def browse_plants
   puts "enter a plant's name to see its traits or enter 'm' to return to the main menu"
-  Plant.all do |plant|
+  Plant.all.each do |plant|
     puts plant.name
   end
   user_input = gets.chomp
   Plant.all.each do |plant|
     if user_input == 'm'
       main_menu
-    elsif user_input == plant
-      puts plant + "traits:"
-      plant.all_traits
+    elsif user_input == plant.name
+      puts "#{plant.name} traits:"
+      plant.all_traits.each do |trait|
+        puts trait.trait
+      end
     end
   end
+  main_menu
 end
 
 def browse_traits
   puts "enter the trait you'd like to look more closely at or enter 'm' to return to the main menu"
-  Trait.all do |trait|
-    puts trait.name
+  Trait.all.each do |trait|
+    puts trait.trait
   end
   user_input = gets.chomp
   Trait.all.each do |trait|
@@ -95,7 +98,65 @@ def add_trait
   main_menu
 end
 
-
+def edit_plant
+  puts "Which plant do you want to edit?"
+  Plant.all.each do |plant|
+    puts plant.name
+  end
+  plant_input = gets.chomp
+  plant_counter = 0
+  Plant.all.each do |plant|
+    if plant_input == plant.name
+      @selected_plant = plant
+      plant_counter += 1
+    end
+  end
+  if plant_counter == 0
+    puts "invalid input, try again"
+    edit_plant
+  end
+  puts "What do you want to do with #{plant_input}?"
+  puts "1) edit plant name"
+  puts "2) add traits"
+  puts "3) remove traits"
+  menu_input = gets.chomp
+  if menu_input == "1"
+    puts "Enter the plant's new name:"
+    new_name = gets.chomp
+    @selected_plant.update(new_name)
+    main_menu
+  elsif menu_input == "2"
+    puts "Which trait would you like to add to this plant?"
+    Trait.all.each_with_index do |trait, index|
+      puts "#{index + 1}) #{trait.trait}"
+    end
+    puts "Enter 'm' to return to main menu"
+    user_input = gets.chomp
+    if user_input == 'm'
+      main_menu
+    end
+    Trait.all.each_with_index do |trait, index|
+      if index == (user_input.to_i - 1)
+        @selected_trait = trait
+      end
+    end
+    new_association = Plant_Trait.new({:plant_id => @selected_plant.id, :trait_id => @selected_trait.id})
+    new_association.save
+    main_menu
+  elsif menu_input == "3"
+    puts "Which trait would you like to remove from this plant?"
+    @selected_plant.all_traits.each_with_index do |trait, index|
+        puts "#{index+1}) #{trait.trait}"
+    end
+    user_input = gets.chomp
+    @selected_plant.all_traits.each_with_index do |trait, index|
+      if index == (user_input.to_i - 1)
+        DB.exec("DELETE FROM plant_traits WHERE trait_id = (#{trait.id}) AND plant_id = (#{@selected_plant.id})")
+      end
+    end
+    main_menu
+  end
+end
 
 
 
